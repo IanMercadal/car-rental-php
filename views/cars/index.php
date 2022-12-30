e429fd0bb222095a16a3ba3dbd51f7a85d797f43
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-12-30 09:40:38 +0100

    feat: :art: Perfeccionando index
    Mejorando estilos y añadiendo comentarios

--- views/cars/index.php
+++ views/cars/index.php
@@ -11,9 +11,6 @@
         </div>
         <div class="banner-img"></div>
     </div>
-</div>
-<!-- OPTIONS -->
-<section class="contenedor">
     <div class="options-example">
         <div class="option-example">
             <div class="option-example-icon">
@@ -31,76 +28,85 @@
         </div>
         <div class="option-example">
             <div class="option-example-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <circle cx="10" cy="10" r="7" />
-                    <line x1="21" y1="21" x2="15" y2="15" />
-                </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="11" r="3" />
+                <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+            </svg>
             </div>
 
             <div class="option-example-text">
-                <p>Search Brand</p>
-                <p><b>BMW</b></p>
+                <p>Location</p>
+                <p><b>Spain</b></p>
             </div>
         </div>
         <div class="option-example">
             <div class="option-example-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <circle cx="10" cy="10" r="7" />
-                    <line x1="21" y1="21" x2="15" y2="15" />
-                </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-wallet" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M17 8v-3a1 1 0 0 0 -1 -1h-10a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1 -1 1h-12a2 2 0 0 1 -2 -2v-12" />
+                <path d="M20 12v4h-4a2 2 0 0 1 0 -4h4" />
+            </svg>
             </div>
 
             <div class="option-example-text">
-                <p>Search Brand</p>
-                <p><b>BMW</b></p>
+                <p>Price Range (Buy)</p>
+                <p><b>60k - 200k</b></p>
             </div>
         </div>
         <div class="option-example">
             <div class="option-example-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <circle cx="10" cy="10" r="7" />
-                    <line x1="21" y1="21" x2="15" y2="15" />
-                </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cash-banknote" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="12" r="3" />
+                <rect x="3" y="6" width="18" height="12" rx="2" />
+                <line x1="18" y1="12" x2="18.01" y2="12" />
+                <line x1="6" y1="12" x2="6.01" y2="12" />
+            </svg>
             </div>
 
             <div class="option-example-text">
-                <p>Search Brand</p>
-                <p><b>BMW</b></p>
+                <p>Price Range (Rent)</p>
+                <p><b>250$ - 500$</b></p>
             </div>
         </div>
+    </div>
+</div>
+<!-- OUR SERVICE -->
+<section class="contenedor">
+    <div class="our-services text-center">
+        <h3>Our services</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, perspiciatis? Unde enim nulla maxime animi quasi quos sapiente, id impedit, accusamus temporibus officiis, neque sunt quo excepturi quod fuga sequi!</p>
     </div>
 </section>
 <!-- ICONOS -->
 <section class="contenedor">
     <div class="iconos">
         <div class="icono">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-steering-wheel" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <circle cx="12" cy="12" r="9" />
-                <circle cx="12" cy="12" r="2" />
-                <line x1="12" y1="14" x2="12" y2="21" />
-                <line x1="10" y1="12" x2="3.25" y2="10" />
-                <line x1="14" y1="12" x2="20.75" y2="10" />
-            </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-steering-wheel" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <circle cx="12" cy="12" r="2" />
+            <line x1="12" y1="14" x2="12" y2="21" />
+            <line x1="10" y1="12" x2="3.25" y2="10" />
+            <line x1="14" y1="12" x2="20.75" y2="10" />
+        </svg>
             <h3>Gran Experiencia</h3>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In hic culpa repellendus nemo possimus atque officia quisquam cumque placeat commodi expedita, nihil deserunt nesciunt vel molestiae vitae veniam repudiandae est.</p>
         </div>
         <div class="icono">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-lock" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <rect x="5" y="11" width="14" height="10" rx="2" />
-                <circle cx="12" cy="16" r="1" />
-                <path d="M8 11v-4a4 4 0 0 1 8 0v4" />
-            </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-lock" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <rect x="5" y="11" width="14" height="10" rx="2" />
+            <circle cx="12" cy="16" r="1" />
+            <path d="M8 11v-4a4 4 0 0 1 8 0v4" />
+        </svg>
             <h3>Garantia de por vida</h3>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In hic culpa repellendus nemo possimus atque officia quisquam cumque placeat commodi expedita, nihil deserunt nesciunt vel molestiae vitae veniam repudiandae est.</p>
         </div>
         <div class="icono">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-discount-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-discount-2" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <line x1="9" y1="15" x2="15" y2="9" />
                 <circle cx="9.5" cy="9.5" r=".5" fill="currentColor" />
                 <circle cx="14.5" cy="14.5" r=".5" fill="currentColor" />
@@ -113,46 +119,58 @@
 </section>
 <!-- CAR LIST -->
 <section class="contenedor">
+    <div id="index-cars" >
+    <h3 class="text-center">Some cars</h3>
     <div class="car-list">
         <div class="car-demo">
-            <h3>BMW Bravus 250cv</h3>
-            <img width="300px" src="<?php echo base_url?>assets/img/bmw.jpg">
-            <div class="caracteristicas">
-                <p><b>Color:</b> Azul</p>
-                <p><b>CV:</b> 250</p>
-                <p><b>Asientos:</b> 5</p>
-            </div>
+            <img class="car-picture" src="<?php echo base_url?>assets/img/bmw.jpg">
+                <p><b>BMW</b></p>
+                <h3>BMW Bravus M3</h3>
+                <div class="caracteristicas">
+                    <p><b>Color:</b> Azul</p>
+                    <p><b>CV:</b> 250</p>
+                    <p><b>Asientos:</b> 5</p>
+                </div>
+                <p><b>Precio:</b> 250$</p>
             <a href="#" class="btn btn-primary">Ver</a>
         </div>
         <div class="car-demo">
-            <h3>BMW Bravus 250cv</h3>
-            <img width="300px" src="<?php echo base_url?>assets/img/bmw.jpg">
-            <div class="caracteristicas">
-                <p><b>Color:</b> Azul</p>
-                <p><b>CV:</b> 250</p>
-                <p><b>Asientos:</b> 5</p>
-            </div>
+            <img class="car-picture" src="<?php echo base_url?>assets/img/bmw.jpg">
+                <p><b>BMW</b></p>
+                <h3>BMW Bravus M3</h3>
+                <div class="caracteristicas">
+                    <p><b>Color:</b> Azul</p>
+                    <p><b>CV:</b> 250</p>
+                    <p><b>Asientos:</b> 5</p>
+                </div>
+                <p><b>Precio:</b> 250$</p>
             <a href="#" class="btn btn-primary">Ver</a>
         </div>
         <div class="car-demo">
-            <h3>BMW Bravus 250cv</h3>
-            <img width="300px" src="<?php echo base_url?>assets/img/bmw.jpg">
-            <div class="caracteristicas">
-                <p><b>Color:</b> Azul</p>
-                <p><b>CV:</b> 250</p>
-                <p><b>Asientos:</b> 5</p>
-            </div>
+            <img class="car-picture" src="<?php echo base_url?>assets/img/bmw.jpg">
+                <p><b>BMW</b></p>
+                <h3>BMW Bravus M3</h3>
+                <div class="caracteristicas">
+                    <p><b>Color:</b> Azul</p>
+                    <p><b>CV:</b> 250</p>
+                    <p><b>Asientos:</b> 5</p>
+                </div>
+                <p><b>Precio:</b> 250$</p>
             <a href="#" class="btn btn-primary">Ver</a>
         </div>
         <div class="car-demo">
-            <h3>BMW Bravus 250cv</h3>
-            <img width="300px" src="<?php echo base_url?>assets/img/bmw.jpg">
-            <div class="caracteristicas">
-                <p><b>Color:</b> Azul</p>
-                <p><b>CV:</b> 250</p>
-                <p><b>Asientos:</b> 5</p>
-            </div>
+            <img class="car-picture" src="<?php echo base_url?>assets/img/bmw.jpg">
+                <p><b>BMW</b></p>
+                <h3>BMW Bravus M3</h3>
+                <div class="caracteristicas">
+                    <p><b>Color:</b> Azul</p>
+                    <p><b>CV:</b> 250</p>
+                    <p><b>Asientos:</b> 5</p>
+                </div>
+                <p><b>Precio:</b> 250$</p>
             <a href="#" class="btn btn-primary">Ver</a>
         </div>
     </div>
+    </div>
+
 </section>
--- views/layout/header.php
+++ views/layout/header.php
@@ -3,10 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Hola</title>
+        <title>Cars</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;500&display=swap" rel="stylesheet"> 
