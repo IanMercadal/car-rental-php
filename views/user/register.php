36c767c1c79322859cae6fbd62dfaabb6c903f34
Ian Mercadal <imercadal@cifpfbmoll.eu>
2023-01-01 11:20:32 +0100

    feat: :art: Mejorando register
    Mejorando estilos

--- views/user/register.php
+++ views/user/register.php
@@ -1,13 +1,56 @@
 <section class="contenedor">
     <h1 class="text-center">Register</h1>
     
-    <form class="form-auth">
-        <input type="text" placeholder="name ">
-        <input type="text" placeholder="surname">
-        <input type="email" placeholder="email">
-        <input type="date">
-        <input type="password">
+    <div class="register">
+        <div class="register-info">
+            <h3>information</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus ex optio ab voluptatibus illo quam quibusdam laborum sed placeat suscipit error, reprehenderit eveniet nemo nisi alias distinctio! Maiores, nulla nemo!</p>
+            <p><b>Lorem ipsum:</b> dolor sit amet consectetur adipisicing elit. Vel accusamus reiciendis, illum sed est dolores quaerat hic quam fugiat deleniti? Cumque suscipit veritatis corrupti aperiam. Quibusdam nobis ullam dolor aliquam!</p>
+            <a class="btn btn-auth">Have an Account</a>
+        </div>
 
-        <button class="btn btn-primary">Register</button>
-    </form>
+        <form class="form-auth">
+            <h3>Form</h3>
+            
+            <div class="form-columns">
+                <div>
+                    <label>Name</label>
+                    <input type="text" placeholder="name">
+                </div>
+
+                <div>
+                    <label>Surname</label>
+                    <input type="text" placeholder="surname">
+                </div>
+            </div>
+
+            <div class="form-columns">
+                <div>
+                    <label>Email</label>
+                    <input type="email" placeholder="email">
+                </div>
+
+                <div>
+                    <label>Date</label>
+                    <input type="date">
+                </div>
+            </div>
+
+            <div class="form-columns">
+                <div>
+                    <label>Password</label>
+                    <input type="password">
+                </div>
+
+                <div>
+                    <label>Confirm Password</label>
+                    <input type="password">
+                </div>
+            </div>
+            
+
+
+            <button class="btn btn-primary">Register</button>
+        </form>
+    </div>
 </section>
